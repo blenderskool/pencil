2722b6e7d1d73017a40bbc878ba43a9259f9cea2
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-06-22 20:08:33 +0530

    Added contents of page generator

--- src/core/functions/content.js
+++ src/core/functions/content.js
@@ -1,9 +1,19 @@
+const regex = /<h([2-3]) id="(.*?)".*?><a.*?>(.*?)<\/a><\/h[2-3]/g;
 
+/**
+ * This function is used to generate the contents of the current page, and adds it
+ * to the {{ index }} hook of the template
+ */
 export default function(data) {
 
-  /**
-   * TODO: Generate contents for the current page.
-   */
+  let index = '<div>Contents</div>';
+  let matches = [];
 
-   return data;
+  while ((matches = regex.exec(data)) !== null) {
+    index += `<div${matches[1] === '3' ? ' class="indent"' : ''}>
+    <a href="#${matches[2]}" aria-hidden="true">${matches[3]}</a>
+    </div>`;
+  }
+
+  return data.replace('{{ index }}', index);
 }
