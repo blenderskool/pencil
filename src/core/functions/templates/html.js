93d043fac6aa27114c13b6eb29c25a1dbb248b27
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-07-06 16:45:42 +0530

    Improved tags creation for <head>

--- src/core/functions/templates/html.js
+++ src/core/functions/templates/html.js
@@ -5,6 +5,7 @@ import addAttributes from '../../utils/attributes';
 export default function(frontMatter) {
   const config = require(__base+'/docbook.config');
 
+  // Gets the HTML template
   let template = '';
   template = fs.readFileSync(path.join(__dirname, '../../templates/html.html')).toString();
 
@@ -12,10 +13,6 @@ export default function(frontMatter) {
   if (frontMatter.lang)
     template = template.replace('lang="en"', `lang="${frontMatter.lang}"`);
 
-  /**
-   * 'meta', 'link' data is taken from the config file and added to the
-   * html template
-   */
 
   // If title is present in meta data for the page, then use it
   if (frontMatter.title)
@@ -31,9 +28,19 @@ export default function(frontMatter) {
    * Adds the theme-color meta tag if themeColor was added in the config file
    * before adding meta tags defined in the config file
    */
-  let tags = config.themeColor ? `<meta name="theme-color" content="${config.themeColor}">` : '';
-  if (Array.isArray(config.head.meta)) {
-    config.head.meta.forEach(metaInfo => {
+  let tags = config.themeColor ?
+  `<meta name="theme-color" content="${config.themeColor}">` : '';
+
+  /**
+   * Creates the <head> section of the site
+   */
+  if (config.head && typeof config.head === 'object') {
+    Object.keys(config.head).forEach(tag => {
+      // If the property is not of Array type
+      if (!Array.isArray(config.head[tag])) return;
+
+      if (tag === 'meta') {
+        return config.head.meta.forEach(metaInfo => {
 
       // Page meta data overrides the global meta data
       if (frontMatter.meta) {
@@ -42,7 +49,12 @@ export default function(frontMatter) {
           delete frontMatter.meta[metaInfo.name];
       }
 
-      tags += addAttributes('meta', metaInfo)+'\n'
+          tags += addAttributes('meta', metaInfo);
+        });
+      }
+
+      config.head[tag].forEach(tagInfo => tags += addAttributes(tag, tagInfo));
+
     });
   }
 
@@ -52,23 +64,21 @@ export default function(frontMatter) {
       tags += addAttributes('meta', {
         name: metaName,
         content: frontMatter.meta[metaName]
-      })+'\n';
+      });
     }
   }
-  template = template.replace('{{ meta }}', tags);
-
-  // Link tags
-  tags = '';
-  if (Array.isArray(config.head.link))
-    config.head.link.forEach(linkInfo => tags += addAttributes('link', linkInfo)+'\n');
-
-  template = template.replace('{{ link }}', tags);
+  // Injects the head tags in the template
+  template = template.replace('{{ head }}', tags);
 
 
   // Header
   if (config.navigation || config.logo) {
     template = template.replace('{{ header }}',
-      `<header>${config.logo ? `<a href="/" class="brand"><img alt="${config.head ? config.head.title : 'Docbook site'}" src=${config.logo}></a>` : ''}<nav>{{ nav }}</nav></header>`
+      `<header>${config.logo ?
+      `<a href="/" class="brand"><img alt="${config.head ?
+      config.head.title :
+      'Docbook site'}" src=${config.logo}></a>` :
+      ''}<nav>{{ nav }}</nav></header>`
     );
     // Adds a class to the container to accomodate for the fixed header height
     template = template.replace('<div class="container">', '<div class="container fixed-head">');
@@ -106,9 +116,9 @@ export default function(frontMatter) {
   template = template.replace('{{ nav }}', tags);
 
   // Scripts for the body section
-  tags = '<script src="/script.js"></script>\n';
+  tags = '<script src="/script.js"></script>';
   if (Array.isArray(config.scripts))
-    config.scripts.forEach(scriptInfo => tags += addAttributes('script', scriptInfo)+'</script>\n');
+    config.scripts.forEach(scriptInfo => tags += addAttributes('script', scriptInfo)+'</script>');
 
   template = template.replace('{{ body }}', tags);
 
