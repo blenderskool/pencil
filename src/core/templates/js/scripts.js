f8242e8674cb22f322782d2b9df427c413c260eb
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-07-11 22:19:57 +0530

    Added session storage usage for storing theme type

--- src/core/templates/js/scripts.js
+++ src/core/templates/js/scripts.js
@@ -7,6 +7,30 @@ function toggleSidebar() {
 function toggleDark(ele) {
   document.body.classList.toggle('dark');
   
+  /**
+   * Change the icon of the button
+   */
   ele.innerHTML =  ele.innerHTML === '<i class="icon ion-ios-sunny"></i>' ?
   '<i class="icon ion-ios-moon"></i>' : '<i class="icon ion-ios-sunny"></i>';
+
+  /**
+   * Store this in session storage
+   */
+  sessionStorage.setItem('docbook-data', JSON.stringify({
+    darkTheme: document.body.classList.contains('dark')
+  }));
 }
+
+/**
+ * Use the session storage data on page load
+ */
+(function() {
+  const data = JSON.parse(sessionStorage.getItem('docbook-data'));
+  if (!data) return;
+
+  if (data.darkTheme)
+    document.body.classList.add('dark');
+  else if (data.darkTheme === false)
+    document.body.classList.remove('dark');
+
+})();
