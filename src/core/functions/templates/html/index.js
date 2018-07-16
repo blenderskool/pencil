8ae45f279cf6b3a0ba98305351a8d07913410efb
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-07-16 20:49:01 +0530

    Set default fallback for themeColor option

--- src/core/functions/templates/html/index.js
+++ src/core/functions/templates/html/index.js
@@ -58,8 +58,7 @@ export default function(frontMatter) {
    * Adds the theme-color meta tag if themeColor was added in the config file
    * before adding meta tags defined in the config file
    */
-  let tags = config.themeColor ?
-  `<meta name="theme-color" content="${config.themeColor}">` : '';
+  let tags = `<meta name="theme-color" content="${config.themeColor ? config.themeColor : '#287BE1'}">`;
 
   /**
    * Creates the <head> section of the site
@@ -72,12 +71,12 @@ export default function(frontMatter) {
       if (tag === 'meta') {
         return config.head.meta.forEach(metaInfo => {
 
-      // Page meta data overrides the global meta data
-      if (frontMatter.meta) {
-        if (frontMatter.meta.hasOwnProperty(metaInfo.name))
-          metaInfo.content = frontMatter.meta[metaInfo.name];
-          delete frontMatter.meta[metaInfo.name];
-      }
+          // Page meta data overrides the global meta data
+          if (frontMatter.meta) {
+            if (frontMatter.meta.hasOwnProperty(metaInfo.name))
+              metaInfo.content = frontMatter.meta[metaInfo.name];
+              delete frontMatter.meta[metaInfo.name];
+          }
 
           tags += addAttributes('meta', metaInfo);
         });
