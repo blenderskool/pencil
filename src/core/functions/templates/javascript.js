9562ac4a1fc734ade0c21b6d8f0b56c0b77932d8
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-08-25 20:45:26 +0530

    Fixes #30

--- src/core/functions/templates/javascript.js
+++ src/core/functions/templates/javascript.js
@@ -2,10 +2,16 @@ import path from 'path';
 import bundler from '../../utils/bundler';
 
 export default function() {
+  /**
+   * Bundle the default, prism, external js files
+   */
+  const config = require(__base+'/docbook.config');
+  const external = Array.isArray(config.scripts) ? config.scripts : [];
+
   return bundler([
     path.join(__dirname, '../../templates/js/scripts.js'),
     path.join(__dirname, '../../templates/js/prism.js')
-  ], err => {
+  ].concat(external), err => {
     console.log(err);
   });
 }
